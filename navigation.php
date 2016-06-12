4906190730c79ffe74688d927bfb9b6b6a7d9725
desairavi1101 <desai.ravi1101@gmail.com>
2016-06-12 10:24:41 +0530

    Added retailer form

--- navigation.php
+++ navigation.php
@@ -6,7 +6,7 @@
                 <div class="mdl-layout-spacer"></div>
                 <!-- Navigation. We hide it in small screens. -->
                 <nav class="mdl-navigation mdl-layout--large-screen-only">
-                    <a class="mdl-navigation__link" href="">Retailer Invoice</a>
+                    <a class="mdl-navigation__link" href="retailer.php">Retailer Invoice</a>
                     <a class="mdl-navigation__link" href="">WholeSaler Invoice</a>
                     <a class="mdl-navigation__link" href="">Search</a>
                     <a class="mdl-navigation__link" href="logout.php">Logout</a>
@@ -16,7 +16,7 @@
         <div class="mdl-layout__drawer no-print">
             <span class="mdl-layout-title">Sagar Auto Parts</span>
             <nav class="mdl-navigation">
-                <a class="mdl-navigation__link" href="">Retailer Invoice</a>
+                <a class="mdl-navigation__link" href="retailer.php">Retailer Invoice</a>
                 <a class="mdl-navigation__link" href="">WholeSaler Invoice</a>
                 <a class="mdl-navigation__link" href="">Search</a>
                 <a class="mdl-navigation__link" href="logout.php">Logout</a>
--- retailer.php
+++ retailer.php
@@ -7,7 +7,7 @@
 <head>
     <title>Sagar Auto Parts</title>
     <!-- Meta Tags-->
-	<meta charset="utf-8" />
+    <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
